238cb37eecaaaabf7ac322a73c84ffdd00b6b6c7
Eduardo Segredo <esegredo@ull.edu.es>
2026-02-24 09:48:58 +0000

    chore: update dependencies and improve documentation
    - Updated ESLint and TypeScript-related dependencies in package.json.
    - Enhanced the add function documentation in src/index.ts.
    - Added sub and div functions with corresponding documentation in src/index.ts.
    - Expanded test cases for add, sub, and div functions in tests/myFunctions.spec.ts.
    - Cleaned up typedoc.json formatting.

--- docs/assets/navigation.js
+++ docs/assets/navigation.js
@@ -1,1 +1,1 @@
-window.navigationData = "eJyLrlYqSa0oUbJSSkxJUdJRKkgsyVCyUkorzUsuyczPK9ZPTEnRyyjJzVHSUcrOzEtRsjIzqY0FAOt9Eco="
+window.navigationData = "eJyLrlYqSa0oUbJSSkxJUdJRKkgsyVCyUkorzUsuyczPK9ZPTEnRyyjJzVHSUcrOzEtRsjIzqdWBa0rJLMOmKSWzDI+m4tIkbJqKS5MwNMUCAJBWNLo="
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,42 @@
 /**
- * Function that adds two numbers
- * @param firstNumber - The first operand of the addition
- * @param secondNumber - The second operand of the addition
- * @returns The addition between `firstOperand` and `secondOperand`
- * 
+ * Adds two __numbers__
+ * @param firstNumber - Consists of the first operand of the addition
+ * @param secondNumber - Consists of the second operand of the addition
+ * @returns The addition of the two numbers `firstNumber` and `secondNumber`
  * ```typescript
- * add(1, 7) // Returns number 8
- * add(3.5, 5.1) // Returns number 8.6
+ * add(1, 7) = 8
  * ```
  */
-
 export function add(firstNumber: number, secondNumber: number): number {
   return firstNumber + secondNumber;
+}
+
+/**
+ * Substracts two __numbers__
+ * @param firstNumber - Consists of the first operand of the substraction
+ * @param secondNumber - Consists of the second operand of the substraction
+ * @returns The substraction of the two numbers `firstNumber` and `secondNumber`
+ * ```typescript
+ * sub(1, 7) = -6
+ * ```
+ *
+ */
+export function sub(firstNumber: number, secondNumber: number): number {
+  return firstNumber - secondNumber;
+}
+
+/**
+ *
+ * @param firstNumber - Consists of the first operand of the division
+ * @param secondNumber - Consists of the second operand of the division
+ * @returns The division of the two numbers `firstNumber` and `secondNumber`
+ * ```typescript
+ * div(4, 8) = 0.5
+ * ```
+ */
+export function div(firstNumber: number, secondNumber: number): number {
+  if (secondNumber === 0) {
+    throw new Error("Zero division");
+  }
+  return firstNumber / secondNumber;
 }
